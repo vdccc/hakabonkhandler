b5bec133159acb6c965640f9cdd47ae28d06c52c
vdccc <kelgam39@gmail.com>
2021-06-21 19:06:56 +0300

    qr code link generator

--- web/price_url_generator.py
+++ web/price_url_generator.py
@@ -1,20 +1,15 @@
 
 class PriceGenerator:
 
-    def __init__(self, dbconnection):
-        self.db = dbconnection
+    def __init__(self):
         self.base_url = "http://127.0.0.1:5000/"
         self.product_l = "c/"
 
     def gen_url(self, product_id, tag_id):
-        args = f"?price={date}"
-        return self.base_url + self.product_l + product_id + args
-
-    def register(self, product_id, tag_id):
-        self.db.exec("INSERT INTO items VALUES (%s, %s);", (product_id, price))
+        query = f"?price={tag_id}"
+        return self.base_url + self.product_l + product_id + query
 
     def gen(self, product_id, tag_id):
         url = self.gen_url(product_id, price)
-        self.register(product_id, price)
 
         return url
